b9d17ff8265482a615f152def174016a593e6a54
kaziibtisumwasit <kaziibtisumwasit@gmail.com>
2026-08-27 14:44:09 +0600

    set timezone asia,add returned time

--- borrowing/models.py
+++ borrowing/models.py
@@ -10,6 +10,6 @@ class Borrowing(models.Model):
     ## one book can borrowed many time, so many(Borrowing) to one(Book) relationship, so we use ForeignKey
     book = models.ForeignKey(Book,on_delete = models.CASCADE,related_name='borrowings') ## if book is deleted, all borrowed books will be deleted
     borrowed_at = models.DateTimeField(auto_now_add=True) ## automatically set the field to now when the object is first created.
-    returned_at = models.DateField(null=True,blank=True)
+    returned_at = models.DateTimeField(null=True,blank=True)
     returned = models.BooleanField(default = False)
     ## related_name is used to access the related objects from the other side of the relationship. For example, if you have a UserProfile object, you can access all the borrowed books of that user using user.borrowings.all() and if you have a Book object, you can access all the users who borrowed that book using book.borrowings.all()
